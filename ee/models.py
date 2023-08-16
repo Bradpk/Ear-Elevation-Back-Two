7ac9aea12767a26162bd73271933b16d97de743e
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-16 17:17:12 +0000

    g

--- ee/models.py
+++ ee/models.py
@@ -7,3 +7,35 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=255)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    date_registered = models.DateTimeField()
+
+class Excercise(models.Model):
+    excercise_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+
+class UserLog(models.Model):
+    log_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    excercise_id = models.ForeignKey(Excercise, on_delete=models.CASCADE)
+    date_completed = models.DateTimeField()
+    total_questions = models.IntegerField()
+    correct_answers = models.IntegerField()
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    excercise_id = models.ForeignKey(Excercise, on_delete=models.CASCADE)
+    audio_text = models.CharField(max_length=255)
+
+class Option(models.Model):
+    option_id = models.AutoField(primary_key=True)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text_option = models.CharField(max_length=255)
+    is_correct = models.BooleanField()
